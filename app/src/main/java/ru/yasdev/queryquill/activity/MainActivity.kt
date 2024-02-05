52a27324103c7b29b5dfa68c88b475037d1d6138
Yasashnyy Maxim <max.yas@list.ru>
2024-02-06 00:09:00 +0300

    feat: Modify topAppBar

--- app/src/main/java/ru/yasdev/queryquill/activity/MainActivity.kt
+++ app/src/main/java/ru/yasdev/queryquill/activity/MainActivity.kt
@@ -1,72 +1,67 @@
 package ru.yasdev.queryquill.activity
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import ru.yasdev.queryquill.components.PinnedTopAppBar
+import androidx.compose.ui.unit.dp
+import ru.yasdev.queryquill.components.MyTopAppBar
 import ru.yasdev.queryquill.ui.theme.QueryQuillTheme
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QueryQuillTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Scaffold(
-                        topBar = { PinnedTopAppBar() }
-                    ) {
-                        Greeting("Android", it)
+                val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+                Scaffold(
+                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                    topBar = {
+                        MyTopAppBar(scrollBehavior = scrollBehavior)
                     }
-
+                ){
+                        MainScreen(paddingValues = it)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, paddingValues: PaddingValues) {
-    Surface(Modifier.padding(paddingValues)) {
 
-            LazyColumn(){
-                items(90) { index ->
+@Composable
+fun MainScreen(paddingValues: PaddingValues){
+
+        Surface(Modifier.padding(top = paddingValues.calculateTopPadding()).fillMaxSize()){
+            LazyColumn{
+                items(60) { index ->
                     Text(text = "qqq $index")
                 }
             }
+        }
 
 
-    }
+
 
 }
 
