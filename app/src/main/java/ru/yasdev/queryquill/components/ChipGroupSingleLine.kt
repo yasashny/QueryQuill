c1a2e77c751882e77b23e218ac795f3453000e08
Yasashnyy Maxim <max.yas@list.ru>
2024-03-07 00:56:50 +0300

    feat: Add ChipGroupSingleLine

---
+++ app/src/main/java/ru/yasdev/queryquill/components/ChipGroupSingleLine.kt
@@ -0,0 +1,45 @@
+package ru.yasdev.queryquill.components
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ChipGroupSingleLine(selectedIndex: MutableState<Int>, options: List<String>, onClick: (Int) -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+            options.forEachIndexed { index, label ->
+                InputChip(
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                    onClick = { onClick(index) },
+                    label = { Text(label) },
+                    selected = index == selectedIndex.value,
+                    leadingIcon = {
+                        if (index == selectedIndex.value){
+                            Icon(
+                                imageVector = Icons.Filled.Done,
+                                contentDescription = "Localized Description",
+                                modifier = Modifier.size(FilterChipDefaults.IconSize)
+                            )
+                        }
+
+                    }
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/ru/yasdev/queryquill/screens/requestScreens/httpRequestScreen/Auth.kt
+++ app/src/main/java/ru/yasdev/queryquill/screens/requestScreens/httpRequestScreen/Auth.kt
@@ -1,15 +1,23 @@
 package ru.yasdev.queryquill.screens.requestScreens.httpRequestScreen
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import ru.yasdev.domain.requestsDb.models.RequestModel
-import ru.yasdev.queryquill.activity.MainActivityViewModel
 import ru.yasdev.queryquill.activity.UpdateHttpRequestModel
+import ru.yasdev.queryquill.components.ChipGroupSingleLine
 import kotlin.reflect.KFunction1
 
 @Composable
 fun Auth(requestModel: RequestModel,
          updateRequest: KFunction1<UpdateHttpRequestModel, Unit>
 ) {
-    Text(text = "Auth")
+    val selectedIndex = remember { mutableStateOf(0) }
+    val options = listOf("No Auth", "Basic Auth")
+    ChipGroupSingleLine(selectedIndex = selectedIndex, options = options) {
+
+        selectedIndex.value = it
+
+    }
+
 }
