8c8d6bd7919de1c3811694fc53378b072ec58c08
Yasashnyy Maxim <max.yas@list.ru>
2024-03-08 23:24:07 +0300

    feat: Add ktor

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("io.ktor:ktor-client-okhttp:2.3.9")
+
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
