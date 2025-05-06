4ca3b2e5cbecbcbb4184a043ab88af5e535e0bfd
Max Yasashny <m.yasashny@gmail.com>
2025-05-07 00:00:02 +0300

    fix: delete unused dependencies from core:data

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -16,8 +16,6 @@
 
 plugins {
     alias(libs.plugins.queryquill.android.library)
-    alias(libs.plugins.queryquill.android.room)
-    alias(libs.plugins.serialization)
 }
 
 android {
@@ -25,14 +23,9 @@ android {
 }
 
 dependencies {
-    implementation(libs.bundles.ktor)
-    implementation(libs.kotlinx.serialization.json)
     implementation(libs.koin.androidx.compose)
-    implementation(libs.datastore.preferences)
-
     implementation(projects.core.model)
     implementation(projects.core.common)
-    implementation(projects.core.utils)
     implementation(projects.core.datastore)
     implementation(projects.core.database)
     implementation(projects.core.network)
